c373519b28f19222763fe541eaa6ef861db3338e
Nicolas Melgar <elnico521@hotmail.com>
2023-07-29 21:51:49 -0300

    primer desarrollo

---
+++ JS/servicios.js
@@ -0,0 +1,3 @@
+alert("Te voy a decir una cosa...el efecto que va a recorrer esta pantalla no lo viste en tu vida. Acepta dice Nicolás");
+alert("Tenía que divertirme un rato...por eso los textos tipo power point...soy un viejo melancólico. Aceptar de nuevo");
+alert("Son este tipo de cosas las que vuelven un sitio único. Te puede no gustar, pero es exlusivo 100%. Aceptar...último");
